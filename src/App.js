423346124a87a4fcae38841130799179b2b3beca
Shyam Joshee <skjoshee007@gmail.com>
2023-03-16 20:04:16 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,47 +1,50 @@
 import React, { useState } from 'react'
 import './App.css';
 
-function ToDo(props) {
-  const {obj, todoValue, setTodoValue} = props;
-  return (<tr>
-    <td>
-      <p>{obj.id}</p>
-    </td>
-    <td>
-      <input value={todoValue} onChange={(event) => setTodoValue(event.target.value)}/>
-    </td>
-    <td>
-      <p>{obj.createdAt}</p>
-    </td>
-  </tr>)
+function ToDo({ todoId, createdAt }) {
+  return (
+    <tr>
+      <td>
+        <p>{todoId}</p>
+      </td>
+      <td>
+        <input placeholder="Enter your task" />
+      </td>
+      <td>
+        <p>{createdAt}</p>
+      </td>
+    </tr>
+  );
 }
 
 function App() {
-  const [todoValue1, setTodoValue1] = useState('');
-  const [todoValue2, setTodoValue2] = useState('');
-  const [todos, setTodos] = useState([{
-    id: 'todo1',
-    createdAt: '20:30',
-  }, {
-    id: 'todo2',
-    createdAt: '18:00',
-  }
+  const [todos, setTodos] = useState([
+    {
+      id: 'todo1',
+      createdAt: '20:30',
+    },
+    {
+      id: 'todo2',
+      createdAt: '18:00',
+    },
   ]);
 
-  const [reverse, setReverse] = useState(true);
+  const reverseOrder = () => {
+    setTodos([...todos].reverse());
+  };
 
   return (
     <div id="main">
-      <button onClick={() => setReverse(!reverse)}>Reverse</button>
+      <button onClick={reverseOrder}>Reverse</button>
       <table>
         <tbody>
-          {reverse ?  <ToDo obj={todos[0]} todoValue={todoValue1} setTodoValue={setTodoValue1}/> : <ToDo obj={todos[1]} todoValue={todoValue2} setTodoValue={setTodoValue2}/>}
-          {reverse ? <ToDo obj={todos[1]} todoValue={todoValue2} setTodoValue={setTodoValue2}/> : <ToDo obj={todos[0]} todoValue={todoValue1} setTodoValue={setTodoValue1}/>}
+          {todos.map((todo) => (
+            <ToDo key={todo.id} todoId={todo.id} createdAt={todo.createdAt} />
+          ))}
         </tbody>
       </table>
     </div>
-  )
+  );
 }
-
 
 export default App;
