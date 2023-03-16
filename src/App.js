bf60b612201cd90fb031eb014c1fa24ab5cea8f6
Shyam Joshee <skjoshee007@gmail.com>
2023-03-16 19:52:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function ToDo(props) {
+  const {obj, todoValue, setTodoValue} = props;
+  return (<tr>
+    <td>
+      <p>{obj.id}</p>
+    </td>
+    <td>
+      <input value={todoValue} onChange={(event) => setTodoValue(event.target.value)}/>
+    </td>
+    <td>
+      <p>{obj.createdAt}</p>
+    </td>
+  </tr>)
 }
+
+function App() {
+  const [todoValue1, setTodoValue1] = useState('');
+  const [todoValue2, setTodoValue2] = useState('');
+  const [todos, setTodos] = useState([{
+    id: 'todo1',
+    createdAt: '20:30',
+  }, {
+    id: 'todo2',
+    createdAt: '18:00',
+  }
+  ]);
+
+  const [reverse, setReverse] = useState(true);
+
+  return (
+    <div id="main">
+      <button onClick={() => setReverse(!reverse)}>Reverse</button>
+      <table>
+        <tbody>
+          {reverse ?  <ToDo obj={todos[0]} todoValue={todoValue1} setTodoValue={setTodoValue1}/> : <ToDo obj={todos[1]} todoValue={todoValue2} setTodoValue={setTodoValue2}/>}
+          {reverse ? <ToDo obj={todos[1]} todoValue={todoValue2} setTodoValue={setTodoValue2}/> : <ToDo obj={todos[0]} todoValue={todoValue1} setTodoValue={setTodoValue1}/>}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
 
 export default App;
